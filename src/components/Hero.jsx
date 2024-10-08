e63f7655820a9095e20ba3efccc8e250301b2f29
aurelwidyanti <aurelwyt@gmail.com>
2024-10-08 22:14:21 +0700

    refactor: Update Hero component responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 import heroImage from "../assets/img/background.png";
 
-const Hero = () => (
-  <section
-    className="hero bg-cover bg-center sm:h-screen flex justify-center items-center rounded-3xl sm:mx-12 sm:my-10"
-    style={{ backgroundImage: `url(${heroImage})`, height: "712px" }}
-  >
-    <div className="flex flex-col sm:w-[1200px] sm:gap-8 text-white text-center">
-      <h1 className="text-7xl font-bold">Wesbeng</h1>
-      <p className="text-2xl font-normal mx-auto">
-        Solusi cerdas untuk pengelolaan sampah anorganik. Lihat jadwal
-        pengangkutan sampahmu,
-        <br />
-        belajar cara daur ulang, dan dapatkan reward menarik.
-        <br />
-        Bergabunglah bersama kami untuk lingkungan yang lebih bersih!
-      </p>
-    </div>
-  </section>
-);
+import { useEffect, useState } from "react";
+
+const Hero = () => {
+  const [height, setHeight] = useState(window.innerWidth >= 640 ? '90vh' : '100vh');
+
+  useEffect(() => {
+    const handleResize = () => {
+      setHeight(window.innerWidth >= 640 ? '90vh' : '100vh');
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <section
+      className="hero bg-cover bg-center flex justify-center items-center rounded-none sm:rounded-3xl sm:mx-12 sm:my-10"
+      style={{
+        backgroundImage: `url(${heroImage})`,
+        height: height,
+      }}
+    >
+      <div className="flex flex-col w-full sm:w-[1200px] gap-4 sm:gap-8 text-white text-center p-4 sm:p-0">
+        <h1 className="text-4xl sm:text-7xl font-bold">Wesbeng</h1>
+        <p className="text-base sm:text-2xl font-normal mx-auto">
+          Solusi cerdas untuk pengelolaan sampah anorganik. Lihat jadwal
+          pengangkutan sampahmu, {' '}
+          <br className="hidden sm:block" />
+          belajar cara daur ulang, dan dapatkan reward menarik.
+          <br className="hidden sm:block" />
+          {' '}Bergabunglah bersama kami untuk lingkungan yang lebih bersih!
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
