28beea3788c93394dfb05652417ed2a51e4d3c8f
aurelwidyanti <aurelwyt@gmail.com>
2024-10-08 20:33:18 +0700

    refactor: Navbar component to add scroll functionality and update styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/img/logo.png";
 
-const Navbar = () => (
-  <header className="flex justify-between items-center sm:px-16 sm:pt-12 bg-transparent text-white fixed w-full top-0 z-10">
-    <div className="flex items-center">
-      <img src={logo} alt="Wesbeng Logo" className="h-12" />
-    </div>
-    <button className="bg-white text-black sm:py-2 sm:px-10 rounded-full font-bold">
-      Login
-    </button>
-  </header>
-);
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // If the page is scrolled more than 50px, set isScrolled to true
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={`flex justify-between items-center sm:px-16 sm:pt-12 fixed w-full top-0 z-10 transition-all duration-300 ${
+        isScrolled
+          ? "bg-[#A0D683] shadow-md text-black"
+          : "bg-transparent text-white"
+      }`}
+    >
+      <div className="flex items-center">
+        <img src={logo} alt="Wesbeng Logo" className="h-12" />
+      </div>
+
+      <button
+        className={`sm:py-2 sm:px-10 rounded-full font-bold transition-all duration-300 ${
+          isScrolled ? "bg-[#2F5150] text-white" : "bg-white text-black"
+        }`}
+      >
+        Login
+      </button>
+    </header>
+  );
+};
 
 export default Navbar;
