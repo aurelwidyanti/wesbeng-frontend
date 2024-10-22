0525d660cc2be0fe9f718ed12ef3d7095f66f0e5
aurelwidyanti <aurelwyt@gmail.com>
2024-10-22 18:58:23 +0700

    refactor: Update Dashboard component layout and add DatePickerComponent

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,27 @@
-import Balance from '../components/dashboard/Balance'
-import Calendar from '../components/dashboard/Calendar'
-import Notification from '../components/dashboard/Notification'
-import Greeting from '../components/dashboard/Greeting'
-import Navbar from '../components/compro/Navbar'
+import Notification from "../components/dashboard/Notification";
+import Greeting from "../components/dashboard/Greeting";
+import Navbar from "../components/compro/Navbar";
+import DatePickerComponent from "../components/ui/DatePickerComponent";
+import Balance from "../components/ui/Balance";
 
 const Dashboard = () => {
   return (
     <>
-    <Navbar />
-    <Greeting />
-    <Balance />
-    <Calendar />
-    <Notification />
+      <Navbar />
+      <div className="mt-[64px] p-8">
+        <Greeting />
+        <div className="flex flex-row gap-4">
+          <div className="flex flex-col gap-4 w-1/2">
+            <Balance />
+            <DatePickerComponent />
+          </div>
+          <div className="w-1/2">
+            <Notification />
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
