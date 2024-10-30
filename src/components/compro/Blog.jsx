f15b3a1d782b15c3c189cb1bef5a821cbeaf5652
rezaaditya16 <rezaramadhanaditya764@gmail.com>
2024-10-30 17:33:30 +0700

    refactor: Simplify Blog component layout and integrate Card component into Carousel

--- src/components/compro/Blog.jsx
+++ src/components/compro/Blog.jsx
@@ -1,31 +1,15 @@
 import { Carousel } from "flowbite-react";
-import { Card } from "flowbite-react";
+// import { Card } from "flowbite-react";
 import "../../assets/img/background.png";
+import Card from "../ui/Card";
 
 const Blog = () => {
   return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel onSlideChange={(index) => console.log('onSlideChange()', index)}>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 1
-        </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 2
-        </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          <Card
-            className="max-w-sm"
-            imgAlt="Meaningful alt text for an image that is not purely decorative"
-            imgSrc="background.png"
-          >
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-            </p>
-          </Card>
-        </div>
+    <div className="h-screen sm:h-[70vh] py-5">
+      <Carousel className="flex items-cente" onSlideChange={(index) => console.log('onSlideChange()', index)}>
+        <Card />
+        <Card />
+        <Card />
       </Carousel>
     </div>
   );
