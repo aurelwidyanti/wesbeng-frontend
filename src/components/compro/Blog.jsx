4edbcdd2eb190c34c4dcf0aed911b24019df4ace
rezaaditya16 <rezaramadhanaditya764@gmail.com>
2024-11-22 21:26:37 +0700

    refactor: Replace static blog cards with dynamic data from educationContent

--- src/components/compro/Blog.jsx
+++ src/components/compro/Blog.jsx
@@ -1,27 +1,19 @@
 import { Carousel } from "flowbite-react";
-// import { Card } from "flowbite-react";
-import dummyImage from '../../assets/img/dummy-image.png';
 import Card from "../ui/Card";
+import blogData from "../../constant/educationContent"
 
 const Blog = () => {
   return (
     <div className="h-screen sm:h-full w-full p-5">
-      <Carousel className="flex items-cente" onSlideChange={(index) => console.log('onSlideChange()', index)}>
-        <Card 
-        image={dummyImage}
-        title="Title 1"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec nunc vel odio aliquet fermentum lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec nunc vel odio aliquet fermentum"
-        />
-        <Card 
-        image={dummyImage}
-        title="Title 2"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec nunc vel odio aliquet fermentum lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec nunc vel odio aliquet fermentum"
-        />
-        <Card
-        image={dummyImage}
-        title="Title 3"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec nunc vel odio aliquet fermentum "
-        />
+      <Carousel className="flex items-center" onSlideChange={(index) => console.log('onSlideChange()', index)}>
+        {blogData.map((blog, index) => (
+          <Card 
+            key={index}
+            image={blog.image}
+            title={blog.title}
+            text={blog.text}
+          />
+        ))}
       </Carousel>
     </div>
   );
---
+++ src/constant/educationContent.js
@@ -0,0 +1,24 @@
+// educationContent.js
+import blog1 from '../assets/img/blog1.jpg';
+import blog2 from '../assets/img/blog2.jpg';
+import blog3 from '../assets/img/blog3.jpg';
+
+const blogData = [
+  {
+    image: blog1,
+    title: "Perlunya Edukasi Sampah Sejak Dini",
+    text: "Sampah menjadi masalah tahunan yang terus meningkat akibat minimnya kesadaran masyarakat terhadap pengelolaan sampah. Kebijakan pemerintah tentang larangan membuang sampah sembarangan sering kali tidak efektif karena kurangnya kepatuhan masyarakat.\n\nDampaknya sangat besar, seperti pencemaran sungai dan laut yang mengancam ekosistem, serta banjir akibat tumpukan sampah di aliran sungai. Contohnya, di kawasan padat penduduk, sampah menyumbat sungai hingga meluap ke pemukiman warga, menyebabkan banjir yang merugikan masyarakat sendiri.\n\nKota besar seperti Jakarta menghadapi banjir tahunan yang mengancam harta benda dan kesehatan warganya. Kesadaran kolektif sangat diperlukan untuk mengatasi masalah ini demi kehidupan yang lebih baik."
+  },
+  {
+    image: blog2,
+    title: "Manajemen Bank Sampah Menjadi Solusi Dalam Pemilahan Sampah Rumah Tangga",
+    text: "Bank sampah memainkan peran penting dalam komersialisasi sampah non-organik dengan menyediakan platform untuk mengumpulkan, memilah, dan menjual material yang dapat didaur ulang.\n\nBank sampah bisa menjalin kerjasama dengan industri daur ulang atau pengepul besar untuk memasok bahan baku. Bank sampah membantu menciptakan nilai ekonomis dari sampah non-organik dan mendorong pemanfaatan kembali sumber daya yang ada."
+  },
+  {
+    image: blog3,
+    title: "APA ITU BANK SAMPAH? DAN APA MANFAATNYA ?",
+    text: "Apa Itu Bank Sampah?\nBank Sampah adalah sistem pengelolaan sampah kering dengan konsep seperti perbankan, di mana masyarakat menabung sampah yang dihargai dengan uang. Sampah ini kemudian dijual ke pabrik atau diolah menjadi kerajinan.\n\nTujuan dan Manfaat Bank Sampah\nBank Sampah bertujuan meningkatkan kesadaran masyarakat untuk mengelola sampah secara ekonomis dan ramah lingkungan. Selain menciptakan lingkungan bersih, program ini juga menambah penghasilan warga. Pemerintah menargetkan pembangunan Bank Sampah di 250 kota untuk mengatasi ancaman sampah yang kian serius."
+  }
+];
+
+export default blogData;
